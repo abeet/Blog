3b54625f8be94799713ac6e6eedb0236378d64ca
wangzhaohui <abeet@163.com>
2019-03-20 17:11:54 +0800

    Update 平衡代码性能及健壮性示例.js

--- 平衡代码性能及健壮性示例.js
+++ 平衡代码性能及健壮性示例.js
@@ -1,3 +1,54 @@
+/** 
+题目：在vue文件第一个标签上标记源文件位置
+**/
+
+/** 
+1：最简实现
+**/
+var source = `<style>
+</style>
+<template>
+<div>
+asdf
+</div>
+</template>
+<script>
+export default {
+}
+</` + `script>`
+var sourcePath = ` data-source="src\\views\\app.vue"`
+var loader = function (source) {
+  return source.replace(/<template>\s+<div/m, '<template>\n<div'+sourcePath)
+}
+alert(['处理前代码:', source, '\n处理后代码:', loader(source)].join('\n'))
+/** 
+2：考虑性能，并考虑非div标签
+**/
+var source = `<style>
+</style>
+<template>
+<span>
+asdf
+</span>
+</template>
+<script>
+export default {
+}
+</` + `script>`
+var sourcePath = ` data-source="src\\views\\app.vue"`
+var loader = function (source) {
+  const templateStartIndex = source.indexOf('<template>')
+  if (templateStartIndex !== -1) {
+    const rootIndex = source.indexOf('>', templateStartIndex + '<template>'.length)
+    return source.substring(0, rootIndex) + sourcePath + source.substring(rootIndex)
+  } else {
+    return source
+  }
+}
+
+/** 
+3：考虑性能，考虑有注释干扰的情况，考虑标签属性中有尖括号的情况
+**/
 var source = `<style>
 </style>
 <template>
@@ -12,46 +63,48 @@ export default {
 }
 </` + `script>`
 function loader(source) {
-	let pos
-	let templateStartIndex = source.indexOf('<template>');
-	let templateEndIndex = source.indexOf('</template>');
-	if (templateStartIndex != -1 && templateEndIndex > templateStartIndex) {
-		pos = templateStartIndex + '<template>'.length;
-		pos = findNextElemPos();
-		return source.substring(0, pos) + ' data-source="src\\views\\app.vue"' + source.substring(pos)
-	} else {
-		return source
-	}
-	function findNextElemPos() {
-		let inElement = false;
-		let inComment = false;
-		while (pos < templateEndIndex) {
-			if (inComment) {
-				if (source.charAt(pos) == '-' && source.charAt(pos + 1) == '-' && source.charAt(pos + 2) == '>') {
-					inComment = false;
-					pos += 3
-				} else {
-					pos++
-				}
-				continue
-			} else if (inElement) {
-				if (source.charAt(pos) == '>' || source.charAt(pos) == ' ') {
-					inElement = false;
-					return pos
-				}
-				pos++
-				continue
-			}
-			if (source.charAt(pos) == '<') {
-				if (source.charAt(pos + 1) == '!' && source.charAt(pos + 2) == '-' && source.charAt(pos + 3) == '-') {
-					pos += 3;
-					inComment = true
-				} else if (source.charAt(pos + 1).match(/\w/)) {
-					inElement = true
-				}
-			}
-			pos++
-		}
-	}
+  let pos
+  let templateStartIndex = source.indexOf('<template>');
+  let templateEndIndex = source.indexOf('</template>');
+  if (templateStartIndex != -1 && templateEndIndex > templateStartIndex) {
+    pos = templateStartIndex + '<template>'.length;
+    pos = findNextElemPos();
+    return source.substring(0, pos) + ' data-source="src\\views\\app.vue"' + source.substring(pos)
+  } else {
+    return source
+  }
+  function findNextElemPos() {
+    let inElement = false;
+    let inComment = false;
+    while (pos < templateEndIndex) {
+      if (inComment) {
+        if (source.charAt(pos) == '-' && source.charAt(pos + 1) == '-' && source.charAt(pos + 2) == '>') {
+          inComment = false;
+          pos += 3
+        } else {
+          pos++
+        }
+        continue
+      } else if (inElement) {
+        if (source.charAt(pos) == '>' || source.charAt(pos) == ' ') {
+          inElement = false;
+          return pos
+        }
+        pos++
+        continue
+      }
+      if (source.charAt(pos) == '<') {
+        if (source.charAt(pos + 1) == '!' && source.charAt(pos + 2) == '-' && source.charAt(pos + 3) == '-') {
+          pos += 3;
+          inComment = true
+        } else if (source.charAt(pos + 1).match(/\w/)) {
+          inElement = true
+        }
+      }
+      pos++
+    }
+  }
 }
 alert(['处理前代码:', source, '\n处理后代码:', loader(source)].join('\n'))
+
+
